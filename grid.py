c1b1e982dd454678cc947d4ecc2cc0c006fc0900
Robin Vincent <robin.vincent@etu.umontpellier.fr>
2024-11-20 16:08:37 +0100

    Ajout de la class grid pour initialiser la grille

---
+++ grid.py
@@ -0,0 +1,10 @@
+class Grid:
+    def __init__(self, rows=6, columns=7):
+        """
+        Initialise une grille vide pour le Puissance 4.
+        :param rows: Nombre de lignes (par défaut 6).
+        :param columns: Nombre de colonnes (par défaut 7).
+        """
+        self.rows = rows
+        self.columns = columns
+        self.grid = [[" " for _ in range(columns)] for _ in range(rows)]
