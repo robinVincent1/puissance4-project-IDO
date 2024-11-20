620235fd493795c557e393e810e3df88b5de795b
Vincent Dubuc <cerise.litchis.0z@icloud.com>
2024-11-20 15:54:02 +0100

    docs(victory): Horizontal victory check added

--- victory_conditions.py
+++ victory_conditions.py
@@ -1,8 +1,13 @@
 class VictoryChecker:
     def __init__(self, grid):
+        """
+                 Initializes victory detector.
+                :param grid: Grid instance.
+                """
         self.grid = grid.grid
 
     def check_horizontal(self, player):
+        """Check horizontal victories"""
         for row in self.grid:
             for col in range(len(row) - 3):
                 if all(cell == player for cell in row[col:col + 4]):
