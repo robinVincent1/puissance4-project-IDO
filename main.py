221bd1473de3d67afe9594fa38ce45b4acd25585
Robin Vincent <robin.vincent@etu.umontpellier.fr>
2024-11-20 16:50:29 +0100

    Ajout de la méthode reset pour réinitialiser la grille

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ if __name__ == "__main__":
     # Initialiser la grille
     grid = Grid()
 
-    # Afficher la grille
-    print("Grille affichée dans la console :")
+    # Afficher la grille initiale
+    print("Grille initiale :")
+    grid.display()
+
+    # Modifier manuellement la grille (pour simuler un jeu)
+    grid.grid[0][0] = "X"
+    grid.grid[1][1] = "O"
+    print("\nGrille modifiée :")
+    grid.display()
+
+    # Réinitialiser la grille
+    grid.reset()
+    print("\nGrille après réinitialisation :")
     grid.display()
